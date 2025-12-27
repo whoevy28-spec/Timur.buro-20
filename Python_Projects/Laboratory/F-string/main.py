0112e8e274bbc3518382444923f1bae7669d6f66
whoevy28-spec <whoevy28@gmail.com>
2025-12-27 15:40:00 +0300

    Added lesson_3 in HTML

--- Python_Projects/Laboratory/F-string/main.py
+++ Python_Projects/Laboratory/F-string/main.py
@@ -1,5 +1,20 @@
-username = "Igor"
-print("Hello", username, "im glad to see u")
-print("Hello ", username, " im glad to see u", sep="")
-print(f"Hello {username} im glad to see u")
-print()
+# username = "Igor"
+# print("Hello", username, "im glad to see u")
+# print("Hello ", username, " im glad to see u", sep="")
+# print(f"Hello {username} im glad to see u")
+# print()
+# i = 10
+# print(f"{i} {id(i)}")
+# for i in range(5):
+#     input()
+#     print(f"Сейчас выполняются действия для i = {i} из диапозона {range(5)}")
+#     print(i * 2)
+#     print(f"{i} {id(i)}")
+
+# print(f"{i} {id(i)}")
+# i = 10
+# print(f"{i} {id(i)}")
+words = "Видео опубликовано только для подписчиков этого телеграмм канала. Если вам понравился подобный формат, дайте знать следующей реакцией".split()
+print(words, type(words))
+for word in words:
+    print(word.strip(".,").lower())
