9bf4cb881bb94c7460d1be9fc6d8ccf8eead5399
whoevy28-spec <whoevy28@gmail.com>
2026-03-07 12:53:47 +0300

    Added python mentor projects

---
+++ Python_Projects/mentor_bootcamp/simple_bmi.py
@@ -0,0 +1,12 @@
+weight = float(input("Ваш вес: "))
+height = float(input("Ваш рост: "))
+bmi = float(weight / (height ** 2))
+if bmi < 18.5:
+    category = "Недостаточная масса тела"
+elif bmi >= 18.5 and bmi <= 24.9:
+    category = "Норма"
+elif bmi >= 25 and bmi <= 29.9:
+    category = "Избыточная масса тела"
+else:
+    category = "Ожирение"
+print(f"Ваш BMI: {bmi} - Категория: {category}")
