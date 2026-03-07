b298cb443235af734e6ac1b8023b30bebc6ef2a3
whoevy28-spec <whoevy28@gmail.com>
2026-03-07 13:23:57 +0300

    fixed task 2 and did if name

--- Python_Projects/mentor_bootcamp/task_002_bmi_functions.py
+++ Python_Projects/mentor_bootcamp/task_002_bmi_functions.py
@@ -1,6 +1,9 @@
 def calculate_bmi(weight, height):
+    # Возвращает индекс массы тела по весу (кг) и росту (м).
     return weight / (height ** 2)
+
 def get_bmi_category(bmi):
+    # Принимает BMI, возвращает категорию (строку).
     if bmi < 18.5:
         return "Недостаточная масса тела"
     elif bmi >= 18.5 and bmi <= 24.9:
@@ -9,8 +12,12 @@ def get_bmi_category(bmi):
         return "Избыточная масса тела"
     else:
         return "Ожирение"
+
 weight = float(input("Ваш вес: "))
 height = float(input("Ваш рост: "))
 bmi = calculate_bmi(weight, height)
 category = get_bmi_category(bmi)
 print(f"Ваш BMI: {bmi:.2f} - Категория: {category}")
+
+if __name__ == "__main__":
+    main()
